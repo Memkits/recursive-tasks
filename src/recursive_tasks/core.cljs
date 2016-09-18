e55d22bd16695bc9a1827af6b91fc3fdadcadbef
jiyinyiyong <jiyinyiyong@gmail.com>
2016-09-18 11:11:10 +0800

    add localStorage

--- src/recursive_tasks/core.cljs
+++ src/recursive_tasks/core.cljs
@@ -1,6 +1,7 @@
 
 (ns recursive-tasks.core
-  (:require [recursive-tasks.schema :as schema]
+  (:require [cljs.reader :refer [read-string]]
+            [recursive-tasks.schema :as schema]
             [recursive-tasks.updater.core :refer [updater]]
             [recursive-tasks.updater.core :refer [updater]]
             [recursive-tasks.updater.core :refer [updater]]
@@ -9,7 +10,12 @@
             [recursive-tasks.comp.container :refer [comp-container]]
             [cljs.reader :refer [read-string]]))
 
-(defonce store-ref (atom schema/store))
+(def storage-key "recursive-tasks")
+
+(defonce store-ref
+ (atom
+   (let [content (.getItem js/localStorage storage-key)]
+     (merge schema/store (if (some? content) (read-string content))))))
 
 (defn dispatch! [op op-data]
   (println "dispatch!" op op-data)
@@ -28,11 +34,16 @@
   (render-app!)
   (println "code update."))
 
+(defn save-store! []
+  (let [content (pr-str @store-ref)]
+    (.setItem js/localStorage storage-key content)))
+
 (defn -main []
   (enable-console-print!)
   (render-app!)
   (add-watch store-ref :changes render-app!)
   (add-watch states-ref :changes render-app!)
+  (.addEventListener js/window "beforeunload" save-store!)
   (println "app started!"))
 
 (set! (.-onload js/window) -main)
